47b2b1598915a85346435e9db7694fd93fe24494
Johan Myrsmeden <johan.myrsmeden@storytelconsultant.com>
2020-11-03 16:09:57 +0100

    Adding template with external button

---
+++ src/routes/simple/Simple.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Button } from 'test-external-svelte-library';
+
+  export let data, helpers;
+</script>
+
+<style>
+  a {
+    margin-bottom: 1rem;
+    display: inline-block;
+  }
+</style>
+
+<svelte:head>
+  <title>{data.title}</title>
+</svelte:head>
+
+<a href="/">&LeftArrow; Home</a>
+
+<h1>{data.title}</h1>
+<Button>Even better if you click here</Button>
+<ul>
+  {#each data.steps as step}
+    <li>{step}</li>
+  {/each}
+</ul>
+
+{@html data.content}
